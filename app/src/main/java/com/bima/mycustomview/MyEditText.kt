0136ee8d846d52e6fdfbbcda16dd3407acdb2965
bimamib <bimaprast1601@gmail.com>
2023-10-02 06:39:41 +0700

    menambahkan init(), showClearButton(), dan hideClearButton()

--- app/src/main/java/com/bima/mycustomview/MyEditText.kt
+++ app/src/main/java/com/bima/mycustomview/MyEditText.kt
@@ -1,20 +1,35 @@
 package com.bima.mycustomview
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
 
-class MyEditText : AppCompatEditText {
+class MyEditText : AppCompatEditText, View.OnTouchListener {
+
+    private lateinit var clearButtonImage: Drawable
 
     constructor(context: Context) : super(context) {
-
+        init()
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-
+        init()
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
 
+    private fun init() {
+        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.baseline_close_24) as Drawable
+        setOnTouchListener(this)
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        return false
     }
 }
