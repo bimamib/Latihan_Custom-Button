eec5e8a52c6a5e1cc68a627359ac355ea5092dec
bimamib <bimaprast1601@gmail.com>
2023-10-02 06:32:32 +0700

    menambahkan constructor

--- app/src/main/java/com/bima/mycustomview/MyEditText.kt
+++ app/src/main/java/com/bima/mycustomview/MyEditText.kt
@@ -1,7 +1,20 @@
 package com.bima.mycustomview
 
+import android.content.Context
+import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
 
 class MyEditText : AppCompatEditText {
 
+    constructor(context: Context) : super(context) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
 }
