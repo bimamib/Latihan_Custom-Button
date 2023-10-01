6a5afa26320af6eae78d03cccada40ede7228afa
bimamib <bimaprast1601@gmail.com>
2023-10-02 06:58:12 +0700

    menambahkan kode untuk aksi ketika tombol clear diklik

--- app/src/main/java/com/bima/mycustomview/MyEditText.kt
+++ app/src/main/java/com/bima/mycustomview/MyEditText.kt
@@ -29,7 +29,63 @@ class MyEditText : AppCompatEditText, View.OnTouchListener {
         setOnTouchListener(this)
     }
 
+    private fun showClearButton() {
+        setButtonDrawables(endOfTheText = clearButtonImage)
+    }
+
+    private fun hideClearButton() {
+        setButtonDrawables()
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+
     override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        if (compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearButtonImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event.x < clearButtonEnd -> isClearButtonClicked = true
+                }
+            } else {
+                clearButtonStart = (width - paddingEnd - clearButtonImage.intrinsicWidth).toFloat()
+                when {
+                    event.x > clearButtonStart -> isClearButtonClicked = true
+                }
+            }
+            if (isClearButtonClicked) {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.baseline_close_24) as Drawable
+                        showClearButton()
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.baseline_close_24) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        return true
+                    }
+                    else -> return false
+                }
+            } else return false
+        }
         return false
     }
 }
