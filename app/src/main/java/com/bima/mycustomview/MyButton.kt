356c7a225df356e6c199d9fd0be16878f19eed3e
bimamib <bimaprast1601@gmail.com>
2023-10-02 05:54:53 +0700

    menambahkan constructor

--- app/src/main/java/com/bima/mycustomview/MyButton.kt
+++ app/src/main/java/com/bima/mycustomview/MyButton.kt
@@ -1,6 +1,19 @@
 package com.bima.mycustomview
 
+import android.content.Context
+import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatButton
 
 class MyButton : AppCompatButton {
+    constructor(context: Context) : super(context) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
 }
