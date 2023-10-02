b145f341b7b5ef93d1de7e85869ae72b4e919769
bimamib <bimaprast1601@gmail.com>
2023-10-02 07:03:33 +0700

    menambahkan kode untuk menampilkan clear button ketika ada perubahan teks

--- app/src/main/java/com/bima/mycustomview/MyEditText.kt
+++ app/src/main/java/com/bima/mycustomview/MyEditText.kt
@@ -2,6 +2,8 @@ package com.bima.mycustomview
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -27,6 +29,20 @@ class MyEditText : AppCompatEditText, View.OnTouchListener {
     private fun init() {
         clearButtonImage = ContextCompat.getDrawable(context, R.drawable.baseline_close_24) as Drawable
         setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
     }
 
     private fun showClearButton() {
