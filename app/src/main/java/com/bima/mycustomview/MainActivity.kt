e28e6fb1c80c8a3f1288bed41c15efc482725b6d
bimamib <bimaprast1601@gmail.com>
2023-10-02 07:39:49 +0700

    menambahkan kode pada MainActivity.kt untuk checking terhadap my_edit_text apakah ada teks atau tidak

--- app/src/main/java/com/bima/mycustomview/MainActivity.kt
+++ app/src/main/java/com/bima/mycustomview/MainActivity.kt
@@ -4,8 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myButton = findViewById(R.id.my_button)
+        myEditText = findViewById(R.id.my_edit_text)
+
+        setMyButtonEnable()
+    }
+
+    private fun setMyButtonEnable() {
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
